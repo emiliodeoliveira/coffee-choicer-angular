39a6867e10f1f63e299e31434cc38be368c70daf
Emilio de Oliveira <emilio.oliveira@safelight.info>
2018-06-11 14:11:45 -0300

    Adicionadas melhorias no código

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Coffee';
+  title = 'Coffee Choicer';
 
 list = ['Daniel','Rodrigo', 'Matheus']
 
   onButtonClick(): void{
     alert('add');
   }
+
+  getUrl()
+{
+  return "url('/assets/img/coffee.jpg')";
+}
+    
 }
